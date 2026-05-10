c27d8a3f229a04a4b7a386b3296d0819a1ffaa71
Shlesh Darji <shleshdarji317@gmail.com>
2026-05-10 20:00:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,11 @@
+
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Install Python dependencies
+# Install dependencies
 pip install -r requirements.txt
 
-# Install Node dependencies and build Tailwind CSS
-npm install
-npm run build:css
-
-# Collect static files
-python manage.py collectstatic --no-input
-
-# Run migrations automatically (Crucial for Free Tier)
+# Run migrations (This is where it usually crashes if psycopg2 is missing)
 python manage.py migrate
+
+python manage.py collectstatic --no-input
