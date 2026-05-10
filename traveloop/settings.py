c954b9fc9a4925d3b5be1b188aac2a348d260ab1
Shlesh Darji <shleshdarji317@gmail.com>
2026-05-10 19:23:21 +0530

    deploy fix

--- traveloop/settings.py
+++ traveloop/settings.py
@@ -92,7 +92,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
