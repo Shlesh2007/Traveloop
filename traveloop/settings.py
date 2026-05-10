0814db3de227217017dc8b1f0ece5ab363161bc5
Shlesh Darji <shleshdarji317@gmail.com>
2026-05-10 18:59:30 +0530

    Update settings.py

--- traveloop/settings.py
+++ traveloop/settings.py
@@ -108,10 +108,17 @@ LOGIN_REDIRECT_URL = "/dashboard/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 # Email settings
+# Email settings - Safe Version
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+# It will look for 'EMAIL_USER' on Render; if not found, it uses your email as a backup.
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER', 'shleshdarji317@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'hucf wzvi ilsv gytz')
+
+# It will look for 'EMAIL_PASSWORD' on Render. 
+# NEVER put the real password string here as a default.
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD') 
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
