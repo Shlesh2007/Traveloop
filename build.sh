f56b704fa3c237147b0dcb43b8428fbccffebf75
Shlesh Darji <shleshdarji317@gmail.com>
2026-05-10 19:47:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
+# Install Python dependencies
 pip install -r requirements.txt
 
+# Install Node dependencies and build Tailwind CSS
 npm install
 npm run build:css
 
+# Collect static files
 python manage.py collectstatic --no-input
 
+# Run migrations automatically (Crucial for Free Tier)
 python manage.py migrate
