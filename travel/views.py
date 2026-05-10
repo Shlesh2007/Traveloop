fa5fa2a8c1e7fb48e825ae0d481a39a4d5dea155
Shlesh Darji <shleshdarji317@gmail.com>
2026-05-10 20:47:27 +0530

    Update views.py

--- travel/views.py
+++ travel/views.py
@@ -361,12 +361,15 @@ def home(request):
         "avg_rating": 4.9
     }
     
-    return render(request, "travel/home.html", {
-        "featured_destinations": featured_destinations,
-        "popular_packages": popular_packages,
-        "search_form": search_form,
-        "stats": stats
-    })
+testimonials = []
+
+return render(request, "travel/home.html", {
+    "featured_destinations": featured_destinations,
+    "popular_packages": popular_packages,
+    "search_form": search_form,
+    "stats": stats,
+    "testimonials": testimonials,
+})
 
 def destination_list(request):
     destinations = Destination.objects.all()
